0e8ebdaf74a1313c586356fbc8da49844aff2fd5
EtudianteAnna <123629810+EtudianteAnna@users.noreply.github.com>
2023-10-20 17:05:37 +0200

    Delete SQLQuery8.sql

--- SQLQuery8.sql
+++
@@ -1,6 +0,0 @@
-SELECT Product.ProductName, VersionVersion.Version, Ticket.NameTicket, Ticket.Description
-FROM Product
-JOIN Ticket ON Product.Id = Ticket.ProductVersionOpSysId
-JOIN VersionVersion ON Product.Id = VersionVersion.Version
-WHERE Ticket.CreatedDate BETWEEN 'DATE_DEBUT' AND 'DATE_FIN'
-AND Ticket.StatusId = 1; -- Utilisez la valeur numérique correspondant à 'En cours'
